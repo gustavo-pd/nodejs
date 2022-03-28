783e96903ca35f5f5cd5bc056c8cf39b5700a43e
GustavoPD <gudalmoro@gmail.com>
2022-03-28 18:15:07 -0300

    metodo all

--- 03.express/api-recipes-crud/index.js
+++ 03.express/api-recipes-crud/index.js
@@ -145,6 +145,11 @@ app.delete('/drinks/:id', function (req, res) {
   res.status(204).end();
 });
 
+app.all('*', function (req, res) {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
+});
+
 app.listen(3001, () => {
   console.log('Aplicação rodando na porta 3001')
 });
+
