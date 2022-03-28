ddee409e111bc58c3242e004af752dd89d596d76
GustavoPD <gudalmoro@gmail.com>
2022-03-28 18:10:52 -0300

    adiciona metodos post, put, delete

--- 03.express/api-recipes-crud/index.js
+++ 03.express/api-recipes-crud/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
@@ -44,11 +46,24 @@ app.get('/recipes', function (_req, res) {
   res.json(recipes);
 });
 
+app.post('/recipes', function (req, res) {
+  const { id, name, price, waitTime } = req.body;
+  recipes.push({ id, name, price, waitTime });
+  res.status(201).json({ message: 'Recipe created successfully!'});
+});
+
+app.get('/validateToken', function (req, res) {
+  const token = req.headers.authorization;
+  if (token.length !== 16) return res.status(401).json({message: 'Invalid Token!'});
+
+  res.status(200).json({message: 'Valid Token!'});
+});
+
 app.get('/recipes/search', function (req, res) {
   const { name, maxPrice, minPrice } = req.query;
   const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < parseInt(maxPrice) && r.price > parseInt(minPrice));
   res.status(200).json(filteredRecipes);
-})
+});
 
 app.get('/recipes/:id', function (req, res) {
   const { id } = req.params;
@@ -59,8 +74,37 @@ app.get('/recipes/:id', function (req, res) {
   res.status(200).json(recipe);
 });
 
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
 app.get('/drinks', function (_req, res) {
   res.json(ordenedDrinks);
+});
+
+app.post('/drinks', function (req, res) {
+  const { id, name, price } = req.body;
+  drinks.push({ id, name, price });
+  res.status(201).json({ message: 'Drink created successfully!'});
 });
 
 app.get('/drinks/search', function (req, res) {
@@ -76,6 +120,29 @@ app.get('/drinks/:id', function (req, res) {
   if (!drink) return res.status(404).json({ message: 'Drink not found!'});
 
   res.status(200).json(drink);
+});
+
+app.put('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const drinksIndex = drinks.findIndex((r) => r.id === parseInt(id));
+
+  if (drinksIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  drinks[drinksIndex] = { ...drinks[drinksIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drinksIndex = drinks.findIndex((r) => r.id === parseInt(id));
+
+  if (drinksIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  drinks.splice(drinksIndex, 1);
+
+  res.status(204).end();
 });
 
 app.listen(3001, () => {
