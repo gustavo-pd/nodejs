c6e8d854bcff18974dd1e956f9d56961e476cb6e
GustavoPD <gudalmoro@gmail.com>
2022-04-08 18:21:39 -0300

    app funcionand

--- 04.model-example-mysql/index.js
+++ 04.model-example-mysql/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
 
-const author = require('./models/Author');
+const Author = require('./models/Author');
 
-app.get('/authors', (req, res) => res.send('Hello world!'));
+app.get('/authors', async (req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
 
 app.listen(port, () => console.log(`Example app listening on PORT ${port}`))
