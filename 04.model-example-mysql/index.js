a0aa7913aa81be9e215cc59d25dc48cfe937497f
GustavoPD <gudalmoro@gmail.com>
2022-04-09 01:34:46 -0300

    books sql'

--- 04.model-example-mysql/index.js
+++ 04.model-example-mysql/index.js
@@ -3,11 +3,18 @@ const app = express();
 const port = 3000;
 
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 app.get('/authors', async (req, res) => {
   const authors = await Author.getAll();
 
   res.status(200).json(authors);
+});
+
+app.get('/books', async (req, res) => {
+  const books = await Book.getAll();
+
+  res.status(200).json(books);
 });
 
 app.listen(port, () => console.log(`Example app listening on PORT ${port}`))
---
+++ 04.model-example-mysql/models/Book.js
@@ -0,0 +1,32 @@
+const connection = require('./connection');
+
+// const newAuthor = ({id, firstName, middleName, lastName}) => {
+//     const fullName = [firstName, middleName, lastName].filter((name) => name).join(" ");
+//     return {
+//         id,
+//         firstName,
+//         middleName,
+//         lastName,
+//         fullName: fullName
+//     }
+// }
+
+// const serialize = (authorData) => {
+//     return {
+//         id: authorData.id,
+//         firstName: authorData.first_name,
+//         middleName: authorData.middle_name,
+//         lastName: authorData.last_name,
+//     }
+// }
+
+const getAll = async () => {
+    const [books] = await connection.execute(
+        'SELECT id, title, author_id FROM model_example.books;',
+    );
+    return books;
+};
+
+module.exports = {
+    getAll,
+};
